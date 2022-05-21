0dc461549f885e90634bda1d62f27da6818c9911
MaxLabLearn <maxlablearn@gmail.com>
2022-05-21 22:22:50 +0300

    feat: solved task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
 
+const newFilesDir = path.resolve(__dirname, 'files-copy');
+const oldFilesDir = path.resolve(__dirname, 'files');
+
+fs.readdir(newFilesDir, (error) => {
+  if (error) {
+    // console.log('Created new directory');
+    fs.mkdir(newFilesDir, (error) => {
+      if (error) {
+        console.error(error.message);
+      }
+    });
+  }
+
+  fs.readdir(oldFilesDir, { withFileTypes: true }, (error, files) => {
+    if (error) {
+      console.error(error.message);
+    } else {
+      files.forEach((file) => {
+        fs.copyFile(path.resolve(oldFilesDir, file.name), path.resolve(newFilesDir, file.name), (error) => {
+          if (error) {
+            console.error(error.message);
+          }
+        });
+      });
+    }
+  });
+});
