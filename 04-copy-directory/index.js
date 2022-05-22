df35612902617d0571e83b9b9a22c5563900784b
MaxLabLearn <maxlablearn@gmail.com>
2022-05-22 09:52:21 +0300

    fix: add update files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,27 +4,30 @@ const path = require('path');
 const newFilesDir = path.resolve(__dirname, 'files-copy');
 const oldFilesDir = path.resolve(__dirname, 'files');
 
-fs.readdir(newFilesDir, (error) => {
-  if (error) {
-    // console.log('Created new directory');
-    fs.mkdir(newFilesDir, (error) => {
-      if (error) {
-        console.error(error.message);
-      }
-    });
-  }
-
-  fs.readdir(oldFilesDir, { withFileTypes: true }, (error, files) => {
+fs.rm(newFilesDir, { recursive: true }, (error) => {
+  if (error) console.log('Created new directory');
+  // console.log('Created new directory');
+  fs.mkdir(newFilesDir, (error) => {
     if (error) {
-      console.error(error.message);
+      console.error('mkdir - ', error.message);
     } else {
-      files.forEach((file) => {
-        fs.copyFile(path.resolve(oldFilesDir, file.name), path.resolve(newFilesDir, file.name), (error) => {
-          if (error) {
-            console.error(error.message);
-          }
-        });
+
+      fs.readdir(oldFilesDir, { withFileTypes: true }, (error, files) => {
+        if (error) {
+          console.error(error.message);
+        } else {
+          files.forEach((file) => {
+            fs.copyFile(path.resolve(oldFilesDir, file.name), path.resolve(newFilesDir, file.name), (error) => {
+              if (error) {
+                console.error(error.message);
+              }
+            });
+          });
+        }
       });
     }
   });
+  
+
+
 });
